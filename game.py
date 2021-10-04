748b1362185adcd4feeefd362a130cd683bba922
EGoldmuse <eli@nishmamusic.com>
2021-10-04 15:25:42 -0500

    first commit

---
+++ game.py
@@ -0,0 +1,10 @@
+import random
+
+guesses_taken = 0
+
+print("Hello, what is your name?")
+myName = input()
+
+number = random.randint(1, 100)
+print("Well, " + myName + " I'm thinking of a number between 1 and 100.")
+
